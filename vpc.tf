e3813d201356a562c68d52377301c241b1033626
Naresh kumar <naresh.kesa19@gmail.com>
2023-09-17 23:47:53 +0530

    provider

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,10 @@
 module "this" {
   source = "../terraform-aws-vpc"
   vpc_cidr = var.cidr_vpc
-  tags     = var.common_tags
+  vpc_tags     = var.common_tags
+  igw_tags = var.common_tags
+  public_subnet_cidr = var.public_subnet_cidr
+  public_subnet_names = var.public_subnet_names
+  private_subnet_cidr = var.private_subnet_cidr
+  private_subnet_names = var.private_subnet_names
 }
