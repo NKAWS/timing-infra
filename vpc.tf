429d1b11efaa0cebdc5d91211327aadcd8e20730
Naresh kumar <naresh.kesa19@gmail.com>
2023-09-17 23:56:53 +0530

    provider

--- vpc.tf
+++ vpc.tf
@@ -7,4 +7,10 @@ module "this" {
   public_subnet_names = var.public_subnet_names
   private_subnet_cidr = var.private_subnet_cidr
   private_subnet_names = var.private_subnet_names
+  public_route_table_tags = merge(
+    var.common_tags,
+    {
+      Name = "timing-public"
+  }
+    )
 }
